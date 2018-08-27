da82b6791ac23056a7f056029ea0bf24468e3d55
Tiago Pádua <tiagolpadua@gmail.com>
2018-08-27 20:42:54 -0300

    Configs

--- src/test/java/com/example/discovery/DiscoveryApplicationTests.java
+++ src/test/java/com/example/discovery/DiscoveryApplicationTests.java
@@ -9,8 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DiscoveryApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {}
 
 }
